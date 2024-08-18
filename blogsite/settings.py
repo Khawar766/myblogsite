96ce7cdbe42938e25b1861042416f3247485e543
Khawar766 <itiskhawar@gmail.com>
2024-08-18 14:39:51 +0500

    new commit

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -106,16 +106,6 @@ WSGI_APPLICATION = 'blogsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': config('DB_NAME'),
-#         'USER': config('DB_USER'),
-#         'PASSWORD': config('DB_PASSWORD'),
-#         'HOST':config('DB_HOST'),
-#         'PORT':config('DB_PORT')
-#     }
-# }
 
 
 # Password validation
